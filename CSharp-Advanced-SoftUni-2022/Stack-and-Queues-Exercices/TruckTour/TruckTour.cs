9c565165fb1276b55f37116824791d002fbc6eaa
llozev91 <llozev91@gmail.com>
2022-05-23 23:01:10 +0200

    Updated TruckTour

--- CSharp-Advanced-SoftUni-2022/Stack-and-Queues-Exercices/TruckTour/TruckTour.cs
+++ CSharp-Advanced-SoftUni-2022/Stack-and-Queues-Exercices/TruckTour/TruckTour.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace TruckTour
 {
@@ -8,10 +10,42 @@ namespace TruckTour
         {
             int n = int.Parse(Console.ReadLine());
 
-            for (int i = 0; i < n - 1; i++)
-            {
+            Queue<string> queue = new Queue<string>();
 
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                input += $" {i}";
+                queue.Enqueue(input);
             }
+
+            int totalFuel = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                string getData = queue.Dequeue();
+                int[] petrolPump = getData.Split().Select(int.Parse).ToArray();
+
+                int fuel = petrolPump[0];
+                int distance = petrolPump[1];
+                totalFuel += fuel;
+
+                if (totalFuel >= distance)
+                {
+                    totalFuel -= distance;
+                }
+                else
+                {
+                    totalFuel = 0;
+                    i = -1;
+                }
+
+                queue.Enqueue(getData);
+            }
+
+            int[] smallestIndex = queue.Dequeue().Split().Select(int.Parse).ToArray();
+
+            Console.WriteLine(smallestIndex[2]);
         }
     }
 }

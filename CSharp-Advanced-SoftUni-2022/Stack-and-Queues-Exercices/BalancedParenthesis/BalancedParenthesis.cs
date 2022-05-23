4cc75547f9d8b1de9176e1f8eccffc4e112d750c
llozev91 <llozev91@gmail.com>
2022-05-23 23:38:42 +0200

    Adding new exercices

---
+++ CSharp-Advanced-SoftUni-2022/Stack-and-Queues-Exercices/BalancedParenthesis/BalancedParenthesis.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BalancedParenthesis
+{
+    internal class BalancedParenthesis
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Stack<char> stack = new Stack<char>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char expectedParentheses = '(';
+                bool isBalanced = false;
+
+                if (input[i] == ')')
+                {
+                    isBalanced = true;
+                }
+                else if (input[i] == ']')
+                {
+                    expectedParentheses = '[';
+                    isBalanced = true;
+                }
+                else if (input[i] == '}')
+                {
+                    expectedParentheses = '{';
+                    isBalanced = true;
+                }
+                else
+                {
+                    stack.Push(input[i]);
+                }
+
+                if (isBalanced)
+                {
+                    if (stack.Count == 0 || stack.Pop() != expectedParentheses)
+                    {
+                        Console.WriteLine("NO");
+                        return;
+                    }
+                }
+            }
+
+            Console.WriteLine("YES");
+        }
+    }
+}
---
+++ CSharp-Advanced-SoftUni-2022/Stack-and-Queues-Exercices/SimpleTextEditor/SimpleTextEditor.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleTextEditor
+{
+    internal class SimpleTextEditor
+    {
+        static void Main(string[] args)
+        {
+            int numberOfCommands = int.Parse(Console.ReadLine());
+
+            StringBuilder textBuilder = new StringBuilder();
+
+            Stack<string> textLastState = new Stack<string>();
+
+            for (int i = 0; i < numberOfCommands; i++)
+            {
+                string[] command = Console.ReadLine().Split();
+
+                if (command[0] == "1")
+                {
+                    textLastState.Push(textBuilder.ToString());
+
+                    string textToAppend = command[1];
+
+                    textBuilder.Append(textToAppend);
+                }
+                else if (command[0] == "2")
+                {
+                    textLastState.Push(textBuilder.ToString());
+
+                    int countOfElements = int.Parse(command[1]);
+
+                    textBuilder.Remove(textBuilder.Length - countOfElements, countOfElements);
+                }
+                else if (command[0] == "3")
+                {
+                    int index = int.Parse(command[1]);
+
+                    Console.WriteLine(textBuilder[index - 1]);
+                }
+                else if (command[0] == "4")
+                {
+                    textBuilder.Clear();
+                    textBuilder.Append(textLastState.Pop());
+                }
+            }
+        }
+    }
+}

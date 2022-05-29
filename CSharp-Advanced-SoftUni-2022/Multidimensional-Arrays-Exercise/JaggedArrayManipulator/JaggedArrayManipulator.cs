dc7e67cc14a8aff20dc7d58f9549c6e7af15f731
llozev91 <llozev91@gmail.com>
2022-05-29 15:21:06 +0200

    Adding new exercises

---
+++ CSharp-Advanced-SoftUni-2022/Multidimensional-Arrays-Exercise/JaggedArrayManipulator/JaggedArrayManipulator.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Linq;
+
+namespace JaggedArrayManipulator
+{
+    internal class JaggedArrayManipulator
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+
+            double[][] jugg = new double[rows][];
+
+            for (int i = 0; i < rows; i++)
+            {
+                jugg[i] = Console.ReadLine().Split().Select(double.Parse).ToArray();
+            }
+
+            for (int i = 0; i < rows - 1; i++)
+            {
+                if (jugg[i].Count() == jugg[i + 1].Count())
+                {
+                    jugg[i] = jugg[i].Select(x => x * 2).ToArray();
+                    jugg[i + 1] = jugg[i + 1].Select(x => x * 2).ToArray();
+                }
+                else
+                {
+                    jugg[i] = jugg[i].Select(x => x / 2).ToArray();
+                    jugg[i + 1] = jugg[i + 1].Select(x => x / 2).ToArray();
+                }
+            }
+
+            string[] command = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            while (command[0] != "End")
+            {
+                string operand = command[0];
+                int row = int.Parse(command[1]);
+                int col = int.Parse(command[2]);
+                double value = double.Parse(command[3]);
+
+                if (!IsCoordinatesValid(jugg, row, col))
+                {
+                    command = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                    continue;
+                }
+
+                if (operand == "Add")
+                {
+                    jugg[row][col] += value;
+                }
+                else if (operand == "Subtract")
+                {
+                    jugg[row][col] -= value;
+                }
+
+                command = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            for (int i = 0; i < rows; i++)
+            {
+                Console.WriteLine(string.Join(" ", jugg[i]));
+            }
+
+        }
+        private static bool IsCoordinatesValid(double[][] matrix, int row, int column)
+        {
+            if (row >= 0 && row < matrix.Length)
+            {
+                if (column >= 0 && column < matrix[row].Length)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ CSharp-Advanced-SoftUni-2022/Multidimensional-Arrays-Exercise/SnakeMoves/SnakeMoves.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Linq;
+
+namespace SnakeMoves
+{
+    internal class SnakeMoves
+    {
+        static void Main(string[] args)
+        {
+            int[] dimensions = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int rows = dimensions[0];
+            int cols = dimensions[1];
+
+            char[,] snakeMatrix = new char[rows, cols];
+
+            string snakeName = Console.ReadLine();
+
+            int currentLetter = 0;
+
+            fillMatrix(snakeMatrix, snakeName, currentLetter);
+
+            printMatrix(snakeMatrix);
+        }
+
+        private static void printMatrix(char[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+
+                Console.WriteLine();
+            }
+        }
+        private static void fillMatrix(char[,] matrix, string snakeName, int currentLetter)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                if (row % 2 == 0)
+                {
+                    for (int col = 0; col < matrix.GetLength(1); col++)
+                    {
+                        matrix[row, col] = snakeName[currentLetter];
+
+                        currentLetter++;
+
+                        if (currentLetter == snakeName.Length)
+                        {
+                            currentLetter = 0;
+                        }
+                    }
+                }
+                else
+                {
+                    for (int col = matrix.GetLength(1) - 1; col >= 0; col--)
+                    {
+                        matrix[row, col] = snakeName[currentLetter];
+
+                        currentLetter++;
+
+                        if (currentLetter == snakeName.Length)
+                        {
+                            currentLetter = 0;
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+}
